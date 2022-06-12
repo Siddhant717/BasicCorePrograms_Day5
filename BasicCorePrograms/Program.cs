26f10f0edb0f3b0c6a92a2b81147a2e4cb4eaa93
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 15:12:12 +0530

    Added the UC3 Solution

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,30 +7,28 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
-            Console.WriteLine("Enter the year :");
-            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the value of n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int power = 1;
+            if (n <= 31)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    power = power * 2;
+                }
+
+                Console.WriteLine("The power of 2= " + power);
+            }
 
 
-            if (year % 400 == 0)
-            {
-                Console.WriteLine("It is a leap year");
 
-            }
-            else if (year % 100 == 0)
-            {
-                Console.WriteLine(" It is not a leap year");
-            }
-            else if (year % 4 == 0)
-            {
-                Console.WriteLine("It is a leap year");
-            }
-            else
-            {
-                Console.WriteLine("It is not a leap year");
-            }
+
+            
         }
-
-
     }
+
+
+    
     
 }
