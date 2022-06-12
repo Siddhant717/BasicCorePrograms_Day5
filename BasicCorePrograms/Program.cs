ff5d0f45e4539bf7fe194620f81ac563c9f75c2c
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 15:04:09 +0530

    Added the UC1 solution

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -8,7 +8,50 @@ namespace BasicCorePrograms
         {
             Console.WriteLine("Welcome to Basic Core Programs");
 
-            
+            int Flip = 100;
+            int HeadCount = 0;
+            int TailCount = 0;
+
+            double HeadPercentage;
+            double TailPercentage;
+
+
+
+            if (Flip > 0)
+            {
+                for (int i = 1; i <= Flip; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next(0, 2);
+                    Console.WriteLine(number);
+
+
+
+                    switch (number)
+                    {
+                        case 0:
+                            TailCount++;
+                            break;
+                        case 1:
+                            HeadCount++;
+                            break;
+                    }
+
+
+                }
+                Console.WriteLine("--------------");
+                HeadPercentage = (HeadCount * 100) / Flip;
+                TailPercentage = (TailCount * 100) / Flip;
+
+                Console.WriteLine("Head Percentage : " + HeadPercentage);
+                Console.WriteLine("Tail Percentage : " + TailPercentage);
+
+            }
+            else
+            {
+                Console.WriteLine("Enter a Positive Integer");
+            }
+
         }
     }
 }
