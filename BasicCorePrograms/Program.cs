70c86c63bb556bf8b094ff422ed5d8bec0f20a93
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 15:31:36 +0530

    Added the UC10 Solution

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -6,28 +6,37 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
-            int n;
-            Console.WriteLine("Enter the number : ");
-            n = Convert.ToInt32(Console.ReadLine());
-            if (n % 2 == 0)
-            {
-                Console.WriteLine("It is a even number.");
-            }
+            int num1, num2, num3;
+            Console.WriteLine("Find the largest among three numbers-");
+            Console.Write("Input the 1st number :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the  2nd number :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the 3rd  number :");
+            num3 = Convert.ToInt32(Console.ReadLine());
 
+
+            if (num1 >= num2 && num1 >= num3)
+            {
+                Console.WriteLine("1st is the largest number.");
+
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                Console.WriteLine("2nd is the largest number.");
+            }
             else
             {
-                Console.WriteLine("It is a odd number.");
+                Console.WriteLine("3rd is the largest number.");
             }
         }
-
-
-
-
-
     }
-    
 
 
-    
-    
 }
+    
+
+
+    
+    
+
